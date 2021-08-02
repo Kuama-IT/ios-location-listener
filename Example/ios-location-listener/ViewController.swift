1957fda381df372f3fa088b4b211869b67e79f9f
Daniele De Matteo <daniele@kuama.net>
2021-08-02 11:44:02 +0200

    Initial commit

---
+++ Example/ios-location-listener/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ios-location-listener
+//
+//  Created by cc51a8303047eb6bf025205b97c4fbcf51205233 on 08/02/2021.
+//  Copyright (c) 2021 cc51a8303047eb6bf025205b97c4fbcf51205233. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
